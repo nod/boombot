615517446acfc191d7b4f54ac8a22da827f2f4c7
Dave Birch <dave@davebir.ch>
2013-08-23 12:02:19 -0500

    adding new quote module

---
+++ plugins/Obama/obamaquotes.py
@@ -0,0 +1,17 @@
+hopenchange = [
+"""Let me say it as simply as I can: transparency and the rule of law will be the touchstones of this presidency""",
+"""Information will not be withheld just because I say so; it will be withheld because a separate authority believes my request is well-grounded in the Constitution.""",
+"""It's time to fundamentally change the way that we do business in Washington. To help build a new foundation for the 21st century, we need to reform our government so that it is more efficient, more transparent, and more creative.""",
+"""This is the most transparent administration in history""",
+"""UPS and FedEx are doing just fine, right? It's the Post Office that's always having problems.""",
+"""The Middle East is obviously an issue that has plagued the region for centuries.""",
+"""I have become a symbol of the possibility of America returning to our best traditions.""",
+"""I do think at a certain point you've made enough money.""",
+"""I believe in keeping guns out of our inner cities, and that our leaders must say so in the face of the gun manufacturer’s lobby.""",
+"""The private sector is doing fine.""",
+"""Let’s not play games. I was suggesting – you’re absolutely right that John McCain has not talked about my Muslim faith.""",
+"""Change doesn't come from Washington. Change comes to Washington""",
+"""I think it’s important to understand that you can’t have 100 percent security and then have 100 percent privacy and zero inconvenience. We’re going to have to make some choices as a society.""",
+"""This Administration also puts forward a false choice between the liberties we cherish and the security we demand… That means no more illegal wire-tapping of American citizens. No more national security letters to spy on citizens who are not suspected of a crime. No more tracking citizens who do nothing more than protest a misguided war. No more ignoring the law when it is inconvenient. That is not who we are. And it is not what is necessary to defeat the terrorists… We will again set an example for the world that the law is not subject to the whims of stubborn rulers, and that justice is not arbitrary. - Obama in 2007""",
+"""Nobody is listening to your telephone calls.""",
+]
